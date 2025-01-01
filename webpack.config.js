0f596fed534d3cb0ddbd1859ae4282cd766eb991
varian97 <variancaesar@gmail.com>
2025-01-01 15:41:10 +0700

    add dev server and bundle everything to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: ["./src/index.ts", "./src/index.css"],
   devtool: "inline-source-map",
-  watch: true,
   devServer: {
     static: {
       directory: __dirname,
     },
     compress: true,
     port: 5000,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
   },
   module: {
     rules: [
@@ -19,8 +28,20 @@ module.exports = {
         include: path.resolve(__dirname, "src"),
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+    }),
+  ],
   resolve: {
     extensions: [".ts", ".js"],
   },
